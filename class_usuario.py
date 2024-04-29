da5899dce13b01679063130b54abbe04f060c7d7
DanielBaumgarten <86086435+DanielBaumgarten@users.noreply.github.com>
2024-04-29 20:28:17 -0300

    <feat>:<incluindo a class Usuarios>

---
+++ class_usuario.py
@@ -0,0 +1,7 @@
+class Usuario:
+    def __init__(self, nome, matricula):
+        self.nome = nome
+        self.matricula = matricula #String
+        self.caixa = None
+
+u = Usuario("Ana", "M11111")
